2b742b9adbe6bb6a74240e076df1cfcf989fe80e
AndrewPochapsky <andrew.pochapsky@gmail.com>
2024-03-22 17:03:40 -0400

    Initialize project with cabal

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,21 @@
---import Structs (StartingConfiguration(StartingConfiguration), Profession)
 module Main where
 
-import System.IO
 import qualified Data.Map as Map
 import Structs
 
 allProfessions :: [Profession]
 allProfessions = [minBound .. maxBound]
 
-myStartingConfiguration :: StartingConfiguration = StartingConfiguration {
+myStartingConfiguration :: StartingConfiguration
+startingState :: State
+
+myStartingConfiguration  = StartingConfiguration {
     startingSize = 100,
     goldPercent = 0.1,
     silverPercent = 0.2,
     ironPercent = 0.7,
     ironProfessionDistribution = ProfessionDistribution (Map.fromList [(Farmer, 0.3), (Baker, 0.2), (Miner, 0.3), (Blacksmith, 0.2)]) }
-startingState :: State = generateStartingState myStartingConfiguration
+startingState = generateStartingState myStartingConfiguration
 
 generateStartingState :: StartingConfiguration -> State
 generateStartingState startingConfiguration = State { citizens =  generateGoldCitizens startingConfiguration ++ generateSilverCitizens startingConfiguration ++ generateIronCitizens startingConfiguration, time = 0 }
@@ -32,11 +33,13 @@ generateIronCitizens :: StartingConfiguration -> [Citizen]
 generateIronCitizens startingConfiguration = concatMap (generateIronCitizensWithProfession startingConfiguration) (Map.toList (getMap $ ironProfessionDistribution startingConfiguration))
 
 generateIronCitizensWithProfession :: StartingConfiguration -> (Profession, Double) -> [Citizen]
-generateIronCitizensWithProfession startingConfiguration (profession, percent) = generateCitizens startingConfiguration (ironPercent startingConfiguration * percent) (const Iron {age = 0, profession = profession})
+generateIronCitizensWithProfession startingConfiguration (professionValue, percent) = generateCitizens startingConfiguration (ironPercent startingConfiguration * percent) (const Iron {age = 0, profession = professionValue})
 
 advance :: State -> State
 advance state = State { citizens = map
     (\citizen -> citizen { age = age citizen + 1 })
     (citizens state), time = time state + 1}
 
+
+main :: IO()
 main = putStrLn "Hello World"
