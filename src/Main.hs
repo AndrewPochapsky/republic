2bca71f10e8e9aacfa6b11dfc346e6da94a9cb7b
AndrewPochapsky <andrew.pochapsky@gmail.com>
2024-03-22 17:23:40 -0400

    Add stylish-haskell

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import qualified Data.Map as Map
-import Structs
+import           Structs
 
 allProfessions :: [Profession]
 allProfessions = [minBound .. maxBound]
@@ -24,10 +24,10 @@ generateCitizens :: StartingConfiguration -> Double -> (() -> Citizen) -> [Citiz
 generateCitizens startingConfiguration percent initFunction = replicate (round (fromIntegral (startingSize startingConfiguration) * percent)) (initFunction ())
 
 generateGoldCitizens :: StartingConfiguration -> [Citizen]
-generateGoldCitizens startingConfiguration = generateCitizens startingConfiguration (goldPercent startingConfiguration) (const Gold { age = 0})
+generateGoldCitizens startingConfiguration = generateCitizens startingConfiguration (goldPercent startingConfiguration) (const Gold { age = 0 })
 
 generateSilverCitizens :: StartingConfiguration -> [Citizen]
-generateSilverCitizens startingConfiguration = generateCitizens startingConfiguration (silverPercent startingConfiguration) (const Silver { age = 0})
+generateSilverCitizens startingConfiguration = generateCitizens startingConfiguration (silverPercent startingConfiguration) (const Silver { age = 0 })
 
 generateIronCitizens :: StartingConfiguration -> [Citizen]
 generateIronCitizens startingConfiguration = concatMap (generateIronCitizensWithProfession startingConfiguration) (Map.toList (getMap $ ironProfessionDistribution startingConfiguration))
--- src/Structs.hs
+++ src/Structs.hs
@@ -3,8 +3,41 @@ module Structs (module Structs) where
 
 import qualified Data.Map as Map
 
-data Profession = Farmer | Baker | Miner | Blacksmith deriving (Show, Ord, Eq, Bounded, Enum)
-data Citizen = Gold { age :: Int } | Silver { age :: Int } | Iron { age :: Int, profession :: Profession } deriving Show
-data State = State { citizens :: [Citizen], time :: Int } deriving Show
-newtype ProfessionDistribution = ProfessionDistribution { getMap::Map.Map Profession Double }
-data StartingConfiguration = StartingConfiguration {startingSize :: Int, goldPercent :: Double, silverPercent :: Double, ironPercent :: Double, ironProfessionDistribution :: ProfessionDistribution }
+data Profession
+  = Farmer
+  | Baker
+  | Miner
+  | Blacksmith
+  deriving (Bounded, Enum, Eq, Ord, Show)
+
+data Citizen
+  = Gold
+      { age :: Int
+      }
+  | Silver
+      { age :: Int
+      }
+  | Iron
+      { age        :: Int
+      , profession :: Profession
+      }
+  deriving (Show)
+
+data State
+  = State
+      { citizens :: [Citizen]
+      , time     :: Int
+      }
+  deriving (Show)
+
+newtype ProfessionDistribution
+  = ProfessionDistribution { getMap :: Map.Map Profession Double }
+
+data StartingConfiguration
+  = StartingConfiguration
+      { startingSize               :: Int
+      , goldPercent                :: Double
+      , silverPercent              :: Double
+      , ironPercent                :: Double
+      , ironProfessionDistribution :: ProfessionDistribution
+      }
